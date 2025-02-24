9396290d5a303608a59854e0959a12fa14f37cbb
hashtagp <hashtag.p27@gmail.com>
2025-02-24 20:47:04 +0530

    Refactor HackathonTracks component to use imported images for better asset management

--- src/components/HackathonTracks/HackathonTracks.jsx
+++ src/components/HackathonTracks/HackathonTracks.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import './HackathonTracks.css';
+import coding from '../../assets/coding.png';
+import secure from '../../assets/secure.png';
+import cyberSecurity from '../../assets/cyber-security.png';
+import iot from '../../assets/iot.png';
+import thinkOutOfTheBox from '../../assets/think-out-of-the-box.png';
 
 const HackathonTracks = () => {
     return (
@@ -7,23 +12,23 @@ const HackathonTracks = () => {
             <h1>Tracks</h1>
             <div className="tracks">
                 <div className="track">
-                    <img src="../../src/assets/coding.png" alt="AI & ML" />
+                    <img src={coding} alt="AI & ML" />
                     <p>AI & ML</p>
                 </div>
                 <div className="track">
-                    <img src="../../src/assets/secure.png" alt="Web3 & Blockchain" />
+                    <img src={secure} alt="Web3 & Blockchain" />
                     <p>Web3 & Blockchain</p>
                 </div>
                 <div className="track">
-                    <img src="../../src/assets/cyber-security.png" alt="Cybersecurity" />
+                    <img src={cyberSecurity} alt="Cybersecurity" />
                     <p>Cybersecurity</p>
                 </div>
                 <div className="track">
-                    <img src="../../src/assets/iot.png" alt="Internet of Things" />
+                    <img src={iot} alt="Internet of Things" />
                     <p>Internet of Things</p>
                 </div>
                 <div className="track">
-                    <img src="../../src/assets/think-out-of-the-box.png" alt="Open Innovation" />
+                    <img src={thinkOutOfTheBox} alt="Open Innovation" />
                     <p>Open Innovation</p>
                 </div>
             </div>
