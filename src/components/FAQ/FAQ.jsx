e7e034d9b25dc59e10d4be8c06ab3789e4095d6d
hashtagp <hashtag.p27@gmail.com>
2025-02-27 22:29:58 +0530

    Update FAQ component: clarify accommodation details for the Grand Finale

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -41,7 +41,7 @@ const FAQ = () => {
     },
     {
       question: 'Will food and accommodation be provided?',
-      answer: 'Yes! For the Grand Finale, food and accommodation is on us. Just bring your ideas and energy!'
+      answer: 'Yes! For the Grand Finale, food is on us. Just bring your ideas and energy!'
     },
   ];
 
